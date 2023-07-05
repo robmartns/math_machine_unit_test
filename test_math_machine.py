a5b57dc06f35a2f2ff3e4f4920e319f08f8d4c6f
Rob Martins <robck2@hotmail.com>
2023-07-04 22:56:39 -0400

    adding results

--- test_math_machine.py
+++ test_math_machine.py
@@ -10,3 +10,12 @@ def math_machine (n1, operator, n2):
         Result = n1 * n2
     else:
         raise Exception(f"Math Machine doesn't recognize '{operator}'")
+    
+    if Result < 0:
+        return 'A negative number. What does that mean?'
+    elif Result < 10:
+        return 'A small number. I can deal with that!'
+    elif Result < 20:
+        return 'A medium-sized number. OK'
+    else:
+        return 'Way too big for me to handle!'
