07d5bbb5ecc62ea05c59d9905cead5acd630b1b9
Rob Martins <robck2@hotmail.com>
2023-07-04 22:42:00 -0400

    adding operators and exception

--- test_math_machine.py
+++ test_math_machine.py
@@ -3,3 +3,10 @@ import pytest
 def math_machine (n1, operator, n2):
     if type(n1) != int or type(n2) != int:
         raise Exception('Only ints are accepted!')
+    
+    if operator == '+':
+        Result = n1 + n2
+    elif operator == '*':
+        Result = n1 * n2
+    else:
+        raise Exception(f"Math Machine doesn't recognize '{operator}'")
