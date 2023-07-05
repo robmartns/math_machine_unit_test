f56b212d26e281542e1029345d95d628fc54a707
Rob Martins <robck2@hotmail.com>
2023-07-04 23:19:31 -0400

    adding test cases

--- test_math_machine.py
+++ test_math_machine.py
@@ -19,3 +19,16 @@ def math_machine (n1, operator, n2):
         return 'A medium-sized number. OK'
     else:
         return 'Way too big for me to handle!'
+    
+class Test_MathMachine:
+    test_non_int_input_for_n1
+    test_non_int_input_for_n2
+    test_addition_with_negative_result
+    test_addition_with_small_result
+    test_addition_with_medium_result
+    test_addition_with_large_result
+    test_multiplication_with_negative_result
+    test_multiplication_with_small_result
+    test_multiplication_with_medium_result
+    test_multiplication_with_large_result
+    test_invalid_operator
